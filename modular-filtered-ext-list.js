7f34f86116cd4b8543d147967cedf8dbd1b05a04
jks05c <jks05c@users.noreply.github.com>
2019-07-17 13:33:20 -0500

    Moved documentation to README

--- modular-filtered-ext-list.js
+++ modular-filtered-ext-list.js
@@ -1,14 +1,3 @@
-/*
-  modular-filtered-ext-list.js
-  Josh Stallings
-  5/7/2016
-
-  Prints results of the module 
-  The function is called with any default arguments it recieves from
-  the module and provides the missing arguments.
-
-*/
-
 var folder = process.argv[ 2 ];
 var ext    = process.argv[ 3 ];
 var myModule = require( './mymodule.js' );
